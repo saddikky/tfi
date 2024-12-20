beb6a192f4e5faabdfae060c431c69a53e125352
saddikky <claribel.pinem@binus.ac.id>
2024-12-20 22:41:08 +0700

    Edited login button and error messages

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -14,28 +14,13 @@
             <div class="card">
                         <img src="{{ asset('images\LOGO-TFI-MERAH-FIXED.png') }}" alt="" class="img">
                         <div class="card-body text-start">
-                            <!-- @if(session('failed')) -->
-                            <!-- <p class="text-danger">{{ session('failed') }}</p> -->
-                            <!-- @endif -->
                             <form action="{{ route('register.submit') }}" method="post">
                                 @csrf
                                 <input type="text" name="name" placeholder="Name" class="form-control mb-3 @error('name') is-invalid @enderror" value="{{ old('name') }}" required>
                                 <input type="text" name="nim" placeholder="NIM" class="form-control mb-3 @error('nim') is-invalid @enderror" value="{{ old('nim') }}" required>
-                                <!-- @error('name')
-                                <div class="class text-danger mb-1">Invalid Name</div>
-                                @enderror -->
                                 <input type="email" name="email" placeholder="BINUSIAN Email" class="form-control mb-3 @error('email') is-invalid @enderror" value="{{ old('email') }}" required>
-                                <!-- @error('email')
-                                    <div class="class text-danger mb-1">Invalid Email</div>
-                                @enderror -->
                                 <input type="password" name="password" placeholder="Password" class="form-control mb-3 @error('password') is-invalid @enderror" required>
-                                <!-- @error('password')
-                                    <div class="class text-danger mb-1">Invalid Password</div>
-                                @enderror -->
                                 <input type="password" name="password_confirmation" placeholder="Confirm Password" class="form-control mb-4 @error('password') is-invalid @enderror" required>
-                                <!-- @error('password')
-                                    <div class="class text-danger mb-1">Invalid Password</div>
-                                @enderror -->
                                 <button type="submit" class="status waiting w-50 mx-auto d-block">CREATE ACCOUNT</button>
                             </form>
 
